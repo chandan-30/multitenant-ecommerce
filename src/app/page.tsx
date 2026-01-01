e5f710cb87c50f90223081492bc0ca8bf981fe78
Sai Chandan <saichandhan.yata44@gmail.com>
2026-01-01 21:28:16 +0530

    02-customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 
 export default function Home() {
   return (
-      <div>
-        <p className="text-rose-500">
-          Hello World!
-        </p>
-      </div>
+        <div className="p-8">
+            <div className="flex flex-col gap-y-4">
+                <div>
+                    <Button variant={"elevated"}>Button</Button>
+                </div>
+                <div>
+                    <Input placeholder="Input" />
+                </div>
+                <div>
+                    <Progress value={50} />
+                </div>
+                <div>
+                    <Textarea value="" placeholder="textarea" />
+                </div>
+                <div>
+                    <Label>Checkbox</Label>
+                    <Checkbox />
+                </div>
+            </div>
+        </div>
     );
 }
